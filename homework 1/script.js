e7cee433068f18f103f19ac7d67150620b8e1de2
Igor_Shumov <92886818+Igor-Shumov@users.noreply.github.com>
2022-01-08 02:34:43 +0300

    js work 100%
    final work for aibol

--- homework 1/script.js
+++ homework 1/script.js
@@ -1,1 +1,50 @@
-'use strict'
+let input = document.querySelector(".inputNumber"),
+  button = document.querySelector(".btn"),
+  selectSort = document.querySelector(".selecter");
+button.addEventListener("click", buttonClick);
+
+function buttonClick() {
+  input.value === ""
+    ? alert("Введите число!")
+    : input.value === "0"
+    ? alert("Так не пойдет!")
+    : getList();
+  input.value = "";
+}
+async function getList() {
+  let res = await fetch(`https://fakestoreapi.com/products` + `?limit=` + `${input.value}`);
+  let data = await res.json();
+  let selector =
+    selectSort.value === "less"
+      ? data.sort(function (a, b) {
+          if (a.price > b.price) {
+            return 1;
+          }
+          if (a.price < b.price) {
+            return -1;
+          }
+          return 0;
+        })
+      : selectSort.value === "more"
+      ? data.sort(function (a, b) {
+          if (a.price < b.price) {
+            return 1;
+          }
+          if (a.price > b.price) {
+            return -1;
+          }
+          return 0;
+        })
+      : data;
+  button.removeEventListener("click", buttonClick);
+  let list = document.querySelector(".posts");
+  for (key in selector) {
+    list.innerHTML += `
+      <div class="post">
+        <h3 class="post_header">${data[key].title}</h3>
+        <p class="price">Price: $ ${data[key].price}</p>
+        <p class="description">Description: ${data[key].description}</p>
+        <img class="image" src="${data[key].image}" width="200">
+      </div>`;
+  }
+}
